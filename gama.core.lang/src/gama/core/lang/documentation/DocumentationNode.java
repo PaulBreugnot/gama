54315dadf79d0616cb55a2495bce12a2064d7993
AlexisDrogoul <alexis.drogoul@gmail.com>
2021-12-23 00:12:32 +0700

    Imports c7d51264ae667e8b1654a0fde08fccfe4ff7747d

--- gama.core.lang/src/gama/core/lang/documentation/DocumentationNode.java
+++ gama.core.lang/src/gama/core/lang/documentation/DocumentationNode.java
@@ -6,134 +6,141 @@
  * (c) 2007-2021 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
  *
  * Visit https://github.com/gama-platform/gama for license information and contacts.
- * 
+ *
  ********************************************************************************************************/
 package gama.core.lang.documentation;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.zip.DeflaterOutputStream;
-import java.util.zip.InflaterInputStream;
 
 import gama.common.interfaces.IGamlDescription;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class DocumentationNode.
  */
 public class DocumentationNode implements IGamlDescription {
 
-	/**
-	 * Compress.
-	 *
-	 * @param text the text
-	 * @return the byte[]
-	 * @throws IOException Signals that an I/O exception has occurred.
-	 */
-	public static byte[] compress(final String text) throws IOException {
-		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		try (final OutputStream out = new DeflaterOutputStream(baos, true);) {
-			out.write(text.getBytes("ISO-8859-1"));
-		}
-		return baos.toByteArray();
-	}
-
-	/**
-	 * Decompress.
-	 *
-	 * @param bytes the bytes
-	 * @return the string
-	 * @throws IOException Signals that an I/O exception has occurred.
-	 */
-	public static String decompress(final byte[] bytes) throws IOException {
-		final InputStream in = new InflaterInputStream(new ByteArrayInputStream(bytes));
-		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		final byte[] buffer = new byte[8192];
-		int len;
-		while ((len = in.read(buffer)) > 0) {
-			baos.write(buffer, 0, len);
-		}
-		return new String(baos.toByteArray(), "ISO-8859-1");
-	}
+	// public static byte[] compress(final String text) throws IOException {
+	// final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	// try (final OutputStream out = new DeflaterOutputStream(baos, true);) {
+	// out.write(text.getBytes("ISO-8859-1"));
+	// }
+	// return baos.toByteArray();
+	// }
+	//
+	// public static String decompress(final byte[] bytes) throws IOException {
+	// final InputStream in = new InflaterInputStream(new ByteArrayInputStream(bytes));
+	// final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	// final byte[] buffer = new byte[8192];
+	// int len;
+	// while ((len = in.read(buffer)) > 0) {
+	// baos.write(buffer, 0, len);
+	// }
+	// return new String(baos.toByteArray(), "ISO-8859-1");
+	// }
+	//
+	// final byte[] title;
+	// final byte[] doc;
 
 	/** The title. */
-	final byte[] title;
-	
+	String title;
+
 	/** The doc. */
-	final byte[] doc;
+	String doc;
 
 	/**
 	 * Instantiates a new documentation node.
 	 *
-	 * @param desc the desc
-	 * @throws IOException Signals that an I/O exception has occurred.
+	 * @param desc
+	 *            the desc
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
 	 */
 	DocumentationNode(final IGamlDescription desc) throws IOException {
-
+		title = desc.getTitle();
+		doc = desc.getDocumentation();
 		final String plugin = desc.getDefiningPlugin();
-		final String title = desc.getTitle();
-		String documentation = desc.getDocumentation();
-		if (plugin != null) {
-			documentation += "\n<p/><i> [defined in " + plugin + "] </i>";
-		}
-		doc = compress(documentation);
-		this.title = compress(title);
+		if (plugin != null) { doc += "\n<p/><i> [defined in " + plugin + "] </i>"; }
 	}
 
 	/**
-	 * Method collectMetaInformation()
+	 * Method collectMetaInformation().
 	 *
-	 * @see gama.common.interfaces.IGamlDescription#collectPlugins(java.util.Set)
+	 * @return the documentation
+	 * @see msi.gama.common.interfaces.IGamlDescription#collectPlugins(java.util.Set)
 	 */
 	// @Override
 	// public void collectMetaInformation(final GamlProperties meta) {}
 
 	@Override
 	public String getDocumentation() {
-		try {
-			return decompress(doc);
-		} catch (final IOException e) {
-			e.printStackTrace();
-			return "Error";
-		}
+		return doc;
+		// try {
+		// return decompress(doc);
+		// } catch (final IOException e) {
+		// e.printStackTrace();
+		// return "Error";
+		// }
 	}
 
+	/**
+	 * Gets the title.
+	 *
+	 * @return the title
+	 */
 	@Override
 	public String getTitle() {
-		try {
-			return decompress(title);
-		} catch (final IOException e) {
-			e.printStackTrace();
-			return "Error";
-		}
+		return title;
+		// try {
+		// return decompress(title);
+		// } catch (final IOException e) {
+		// e.printStackTrace();
+		// return "Error";
+		// }
 	}
 
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
 	@Override
-	public String getName() {
-		return "Online documentation";
-	}
+	public String getName() { return "Online documentation"; }
 
+	/**
+	 * Gets the defining plugin.
+	 *
+	 * @return the defining plugin
+	 */
 	@Override
-	public String getDefiningPlugin() {
-		return "";
-	}
+	public String getDefiningPlugin() { return ""; }
 
+	/**
+	 * Sets the name.
+	 *
+	 * @param name the new name
+	 */
 	@Override
 	public void setName(final String name) {
 		// Nothing
 	}
 
+	/**
+	 * To string.
+	 *
+	 * @return the string
+	 */
 	@Override
 	public String toString() {
 		return getTitle() + " - " + getDocumentation();
 	}
 
 	/**
-	 * Method serialize()
+	 * Method serialize().
 	 *
-	 * @see gama.common.interfaces.IGamlable#serialize(boolean)
+	 * @param includingBuiltIn the including built in
+	 * @return the string
+	 * @see msi.gama.common.interfaces.IGamlable#serialize(boolean)
 	 */
 	@Override
 	public String serialize(final boolean includingBuiltIn) {
