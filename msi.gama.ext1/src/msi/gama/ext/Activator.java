b3c3a465f38f89d7c71fb932750599d760737656
hqnghi88 <hqnghi88@gmail.com>
2022-07-07 23:51:32 +0700

    split ext by 2

---
+++ msi.gama.ext1/src/msi/gama/ext/Activator.java
@@ -0,0 +1,62 @@
+/*********************************************************************************************
+ *
+ *
+ * 'Activator.java', in plugin 'msi.gama.headless', is part of the source code of the GAMA modeling and simulation
+ * platform. (v. 1.8.1)
+ *
+ * (c) 2007-2020 UMI 209 UMMISCO IRD/UPMC & Partners
+ *
+ * Visit https://github.com/gama-platform/gama for license information and developers contact.
+ *
+ *
+ **********************************************************************************************/
+package msi.gama.ext;
+
+import javax.imageio.ImageIO; 
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+ 
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework. BundleContext)
+	 */
+	@Override
+	public void start(final BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		// Forces early initialisation of operation registry of JAI. It fixes initialisation problems in some third
+		// party equinox x@applications such as OpenMOLE.
+ 
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(final BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+	// See DEBUG.java
+	public static String PAD(final String string, final int minLength, final char pad) {
+		if (string.length() >= minLength) return string;
+		final StringBuilder sb = new StringBuilder(minLength);
+		sb.append(string);
+		for (int i = string.length(); i < minLength; i++) {
+			sb.append(pad);
+		}
+		return sb.toString();
+	}
+
+}
